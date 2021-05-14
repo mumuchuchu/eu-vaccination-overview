34cb7411fb5ef14f43b9c7f6dc0702675ca384c6
muchu <muchu@noheroes.nl>
2021-05-15 01:25:21 +0200

    split steps into functions and check __name__

--- main.py
+++ main.py
@@ -3,35 +3,38 @@ from utility import get_country_list, get_eu_overview
 from eu_map import get_map_template
 from datetime import date
 
-# fetch data (json) from website
+def read_data_from_web(url):
+    try:
+        response = requests.get(url, allow_redirects=True)
+        response.raise_for_status()
+    except Exception as e:
+        print("Could not fetch data, see error message: ", e)
+    else:
+        return response.json()
 
-url = 'https://opendata.ecdc.europa.eu/covid19/vaccine_tracker/json'
-
-try:
-    response = requests.get(url, allow_redirects=True)
-    data = response.json()
-except Exception as e:
-    print("Could not fetch data, see error message: ", e)
-else:
-        
-    # sorting data for Netherlands
+def process_data(data):
     records = (data["records"])
     countries_list = []
     eu_data_list = []
-    country = 'NL'
-
-    get_country_list(records, countries_list)
-    get_eu_overview(records, countries_list, eu_data_list)
     today = date.today().strftime("%b %d %Y")
 
-    country_data = get_overview_by_contry(country, eu_data_list)
-    country_vaccination_pct = round(int(country_data["data"]["total dose"])/int(country_data["data"]["population"])*100)
-    country_not_vaccination_pct = 100 - country_vaccination_pct
+    get_country_list(records, countries_list)
+    overview_data = get_eu_overview(records, countries_list, eu_data_list)
+     # Generate map in html
+    output = get_map_template(today, overview_data)
+    return output
 
-        # Generate map in html
-        map = get_map_template(today, eu_data_list)
+def write_data_to_file(data):
+    with open('index.html','w') as f:
+        html = data
+        f.write(html)
 
-        # Output html file
-        with open('index.html','w') as f:
-            html = map
-            f.write(html)
+def main():
+    # fetch data (json) from website
+    url = 'https://opendata.ecdc.europa.eu/covid19/vaccine_tracker/json'
+    data = read_data_from_web(url)
+    map = process_data(data)
+    write_data_to_file(map)
+
+if __name__ == '__main__':
+    main()
