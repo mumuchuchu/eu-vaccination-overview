c6d48e99d0aed6244829457945c3754b97a18f1d
muchu <muchu@noheroes.nl>
2021-05-15 00:15:27 +0200

    make main file more readable and move map html to a separate place

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
+from utility import get_country_list, get_eu_overview
+from eu_map import get_map_template
 from datetime import date
-from utility import get_country_list, get_eu_overview, get_overview_by_contry
 
 # fetch data (json) from website
 
@@ -27,50 +28,10 @@ else:
     country_vaccination_pct = round(int(country_data["data"]["total dose"])/int(country_data["data"]["population"])*100)
     country_not_vaccination_pct = 100 - country_vaccination_pct
 
-    # Output html file
+        # Generate map in html
+        map = get_map_template(today, eu_data_list)
 
-    with open('index.html','w') as f:
-
-        html = '''<!DOCTYPE html>
-            <html lang="en">
-            <head>
-                <meta charset="UTF-8">
-                <meta http-equiv="X-UA-Compatible" content="IE=edge">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <link rel="stylesheet" href="style.css">
-                <title>%s Vaccination Rate</title>
-            </head>
-            <body>
-                <h1 style="text-align: center">Date: %s</h1>
-                <div class="chart_wrapper">
-                    <div class="chart">
-                        <h2>%s Vaccination Rate</h2>
-                        <h3>Amount: %s</h3>
-                        <h3>Percentage: %d%%</h3>
-                        <div class="chart__container">
-                            <div class="chart__inner-container">
-                                <svg viewBox="0 0 64 64" class="pie" style="background: #c4c4c4">
-                                    <circle
-                                    class="absent-docs"
-                                    r="25%%"
-                                    cx="50%%"
-                                    cy="50%%"
-                                    style="stroke-dasharray: %d 100"
-                                    ></circle>
-                                    <circle
-                                    class="available-docs"
-                                    r="25%%"
-                                    cx="50%%"
-                                    cy="50%%"
-                                    style="stroke-dasharray: %d 100; stroke-dashoffset: -%d"
-                                    ></circle>
-                                </svg>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </body>
-            </html>'''
-
-        content = html % (country, today, country, country_data["data"]["total dose"], country_vaccination_pct, country_vaccination_pct, country_not_vaccination_pct, country_vaccination_pct) 
-        f.write(content)
+        # Output html file
+        with open('index.html','w') as f:
+            html = map
+            f.write(html)
